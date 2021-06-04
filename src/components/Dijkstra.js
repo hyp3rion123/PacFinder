96922c5da7c37a87cb9f684f8ffbc950721ccb5b
hyp3rion123 <andreigli@yahoo.com>
2021-06-04 16:14:45 -0400

    First Commit - Dijkstra

---
+++ src/components/Dijkstra.js
@@ -0,0 +1,165 @@
+//The Dijkstra function will return an array of objects of the format {x: xpos, y: ypos, state: currentState} to indicate the next tile and what animation to render.
+// currentState will be one of: "explored"(the "visited" tiles), "exploring"(the current tile being examined), "path" (one of the tiles that leads the start node to the exit node
+// through an optimal path calculated by the algorithm)
+//The grid will receive this array instantly and then render the visualization. In other words, the visualization is pre-calculated.
+
+class MinHeap { //data structure for keeping track of unvisited nodes and the order in which they are visited
+    constructor(root) {
+        this.heap=[root]
+    }
+    getRoot = () => this.heap[0];
+
+    length = () => this.heap.length;
+
+    getArrayForm = () => this.heap;
+
+    insertNode = (node) => {
+        if (this.heap.length === 0) {
+            this.heap.push(node)
+        } else {
+            this.heap.push(node); //inserts at next available leaf node
+            let currIndex = this.heap.length - 1;
+            let parentIndex = Math.floor((currIndex - 1) / 2);
+            while(parentIndex >= 0 && this.heap[parentIndex].distance > this.heap[currIndex].distance) { //fix heap
+                //SWAP                
+                [this.heap[parentIndex], this.heap[currIndex]] = [this.heap[currIndex], this.heap[parentIndex]];
+                currIndex = parentIndex;
+                parentIndex = Math.floor((currIndex - 1) / 2);
+            }
+        }
+    }
+
+    deleteRoot = () => {
+        if (this.heap.length === 0) {
+            return;
+        } else if (this.heap.length === 1) {
+            this.heap.length = 0;
+        } else {
+           this.heap[0] = this.heap[this.heap.length - 1];
+           this.heap.pop();
+           let currIndex = 0;
+           let leftChildIndex = (currIndex * 2) + 1;
+           let rightChildIndex = (currIndex * 2) + 2;
+           let leftChildExists = leftChildIndex < this.heap.length;
+           let rightChildExists = rightChildIndex < this.heap.length;
+           while((leftChildExists && this.heap[currIndex].distance > this.heap[leftChildIndex].distance) || 
+                (rightChildExists && this.heap[currIndex].distance > this.heap[rightChildIndex].distance)){ //fix heap after deletion
+                    let swappingWithLeft = true; //true: swap with left, false: swap with right
+                    if (rightChildExists && this.heap[rightChildIndex].distance < this.heap[leftChildIndex].distance) {
+                        swappingWithLeft = false;
+                    }
+
+                    if (swappingWithLeft) {
+                        [this.heap[currIndex], this.heap[leftChildIndex]] = [this.heap[leftChildIndex], this.heap[currIndex]];
+                        currIndex = leftChildIndex;                        
+                    } else {
+                        [this.heap[currIndex], this.heap[rightChildIndex]] = [this.heap[rightChildIndex], this.heap[currIndex]];
+                        currIndex = rightChildIndex;   
+                    }
+
+                    leftChildIndex = (currIndex * 2) + 1;
+                    rightChildIndex = (currIndex * 2) + 2;
+                    leftChildExists = leftChildIndex < this.heap.length;
+                    rightChildExists = rightChildIndex < this.heap.length;
+                }
+        }
+    }
+}
+    
+export const dijkstra = async (startX, startY, endX, endY, grid) => {
+    let moves = [];// unvisited = [];
+    let Heap = new MinHeap(grid[startY][startX]);    
+
+    grid.map(row => {
+        row.map(tile => {
+            if (tile.x !== startX || tile.y !== startY) {
+                tile.distance = Infinity;
+            }            
+            tile.tileState = "unexplored";
+        })
+    })
+
+    const getNeighbours = (x,y) => {
+        const width = grid[0].length;
+        const height = grid.length;
+        let neighbours = [], unvisited = Heap.getArrayForm();
+        //UP
+        if (y - 1 >= 0 && !grid[y-1][x].wall && grid[y-1][x].tileState.localeCompare("explored") !== 0 && !grid[y-1][x].start && !unvisited.includes(grid[y-1][x])) {
+            neighbours.push(grid[y-1][x])
+        }
+        //RIGHT
+        if (x + 1 < width && !grid[y][x+1].wall && grid[y][x+1].tileState.localeCompare("explored") !== 0 && !grid[y][x+1].start && !unvisited.includes(grid[y][x+1])) {
+            neighbours.push(grid[y][x+1])
+        }
+        //DOWN
+        if (y + 1 < height && !grid[y+1][x].wall && grid[y+1][x].tileState.localeCompare("explored") !== 0 && !grid[y+1][x].start && !unvisited.includes(grid[y+1][x])) {
+            neighbours.push(grid[y+1][x])
+        }
+        //LEFT
+        if (x - 1 >= 0 && !grid[y][x-1].wall && grid[y][x-1].tileState.localeCompare("explored") !== 0 && !grid[y][x-1].start && !unvisited.includes(grid[y][x-1])) {
+            neighbours.push(grid[y][x-1])
+        }
+        return neighbours;
+    }
+
+    let neighbours = getNeighbours(startX, startY);
+    for(let neighbour in neighbours) {
+        neighbours[neighbour].distance = 1;
+        neighbours[neighbour].previousTile = grid[startY][startX];
+        //unvisited.push(neighbours[neighbour]);
+        Heap.insertNode(neighbours[neighbour]);
+    }    
+    Heap.deleteRoot();
+    
+    //Heap currently contains the (up to) 4 neighbours of start node
+    const explore = () => {
+        while(grid[endY][endX].tileState.localeCompare("explored") !== 0 && Heap.length() > 0) {
+            const toExplore = Heap.length();
+            for (let i = 0; i < toExplore; i++) {
+                let root = Heap.getRoot();
+                root.tileState = "exploring";
+                moves.push({x: root.x, y: root.y, state: "exploring"});
+                const neighbours = getNeighbours(root.x, root.y);
+                
+                if (neighbours.includes(grid[endY][endX])){ //if end has been found, start exiting the loop
+                    if(grid[endY][endX].distance > root.distance + 1) {
+                        grid[endY][endX].distance = root.distance + 1;
+                        grid[endY][endX].previousTile = root;
+                    }
+                    let currentTile = grid[endY][endX].previousTile;
+                    console.log("currentile: ", currentTile);
+                    while(currentTile.x !== startX || currentTile.y !== startY) {
+                        currentTile.tileState = "path";
+                        moves.push({x: currentTile.x, y: currentTile.y, state: "path"});
+                        currentTile = currentTile.previousTile;
+                    }
+                    return;
+                }
+                for (let neighbour in neighbours) {                
+                    if (neighbours[neighbour].distance > root.distance + 1){
+                        neighbours[neighbour].distance = root.distance + 1;
+                        neighbours[neighbour].previousTile = root;
+                    }
+                    //unvisited.push(neighbours[neighbour]);
+                    Heap.insertNode(neighbours[neighbour])
+                }                
+                const currHeap = Heap.getArrayForm();
+                root.tileState = "explored";
+                moves.push({x: root.x, y: root.y, state: "explored"});
+                Heap.deleteRoot();               
+            }
+            //unvisited = unvisited.slice(toExplore - 1);
+        }
+        
+        let currentTile = grid[endY][endX].previousTile;
+        while(currentTile && (currentTile.x !== startX || currentTile.y !== startY)) {
+            currentTile.tileState = "path";
+            moves.push({x: currentTile.x, y: currentTile.y, state: "path"});
+            currentTile = currentTile.previousTile;
+        }
+    }
+    explore();
+    return moves;    
+}  
+
+
---
+++ src/components/Grid.js
@@ -0,0 +1,206 @@
+import React, { Component } from "react";
+import Tile from "./Tile";
+import {dijkstra} from './Dijkstra'
+import "./Grid.css";
+
+export default class Grid extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentGridTiles: [],
+      isHoldingWall: false,
+      isHoldingStart: false,
+      isHoldingEnd: false,
+      currentStartingPosition: { x: 19, y: 7 },
+      currentEndingPosition: { x: 29, y: 7 },
+    };
+  }
+  width = 50; //not part of state because dimensions are fixed..for now
+  height = 15;
+
+  componentDidMount() {
+    const emptyTileArray = this.generateEmptyTileArray();
+    this.setState({ currentGridTiles: emptyTileArray });
+  }
+
+  handleMouseEnter = (x, y, e) => {
+    console.log("start: ", this.state.currentStartingPosition, "end: ", this.state.currentEndingPosition)
+    e.preventDefault();
+    if (this.state.isHoldingStart === true && !(x === this.state.currentEndingPosition.x && y === this.state.currentEndingPosition.y)) {
+      let updatedArray = this.updatedTileArray(x, y, "start");
+      this.setState({ currentGridTiles: updatedArray, isHoldingStart: true });
+    } else if (this.state.isHoldingEnd === true && !(x === this.state.currentStartingPosition.x && y === this.state.currentStartingPosition.y)) {
+      let updatedArray = this.updatedTileArray(x, y, "end");
+      this.setState({ currentGridTiles: updatedArray, isHoldingEnd: true });
+    } else if (this.state.isHoldingWall === true && !(x === this.state.currentStartingPosition.x && y === this.state.currentStartingPosition.y) && !(x === this.state.currentEndingPosition.x && y === this.state.currentEndingPosition.y)) {
+      let updatedArray = this.updatedTileArray(x, y, "wall");
+      this.setState({ currentGridTiles: updatedArray, isHoldingWall: true });
+    }
+  };
+
+  handleMouseDown = (x, y, e) => {
+    e.preventDefault();
+    if (x === this.state.currentStartingPosition.x && y === this.state.currentStartingPosition.y) {
+      this.setState({ isHoldingStart: true })
+    } else if (x === this.state.currentEndingPosition.x && y === this.state.currentEndingPosition.y) {
+      this.setState({ isHoldingEnd: true })
+    } else {
+      let updatedArray = this.updatedTileArray(x, y, "wall");
+      this.setState({ currentGridTiles: updatedArray, isHoldingWall: true });
+    }    
+  };
+
+  handleMouseUp = (e) => {
+    e.preventDefault();
+    this.setState({ isHoldingWall: false, isHoldingStart: false, isHoldingEnd: false });
+  };
+
+  updatedTileArray(x, y, cmd) {
+    let updatedArray = this.state.currentGridTiles.slice();
+    if (!cmd.localeCompare("start") && !updatedArray[y][x].wall) { //make the origninal start a regular empty tile
+      updatedArray[this.state.currentStartingPosition.y][this.state.currentStartingPosition.x] = {
+        ...updatedArray[this.state.currentStartingPosition.y][this.state.currentStartingPosition.x],
+        start: false,
+      }
+      updatedArray[y][x] = {
+        ...updatedArray[y][x],
+        start: true,
+      }
+      this.setState({ currentStartingPosition: { x: x, y: y }});
+    } else if (!cmd.localeCompare("end") && !updatedArray[y][x].wall) { //make the origninal end a regular empty tile
+      updatedArray[this.state.currentEndingPosition.y][this.state.currentEndingPosition.x] = {
+        ...updatedArray[this.state.currentEndingPosition.y][this.state.currentEndingPosition.x],
+        end: false,
+      }
+      updatedArray[y][x] = {
+        ...updatedArray[y][x],
+        end: true,
+      }
+      this.setState({ currentEndingPosition: { x: x, y: y }});
+    } else {
+      updatedArray[y][x] = {
+        ...updatedArray[y][x],
+        wall: !cmd.localeCompare("wall") ? !this.state.currentGridTiles[y][x].wall: this.state.currentGridTiles[y][x].wall,
+        path: !cmd.localeCompare("path") ? true : false,
+        exploring: !cmd.localeCompare("exploring") ? true : false,
+        explored: !cmd.localeCompare("explored") ? true : false,
+      };
+    }
+    return updatedArray;
+  }
+
+  generateEmptyTileArray = () => {
+    let tileArray = [];
+    for (let yRow = 0; yRow < this.height; yRow++) {
+      tileArray[yRow] = [];
+      for (let xCol = 0; xCol < this.width; xCol++) {
+        const start = this.state.currentStartingPosition;
+        const end = this.state.currentEndingPosition;
+        tileArray[yRow][xCol] = {
+          key: yRow * this.width + xCol,
+          x: xCol,
+          y: yRow,
+          handleMouseEnter: this.handleMouseEnter,
+          handleMouseDown: this.handleMouseDown,
+          handleMouseUp: this.handleMouseUp,
+          wall: false,
+          path: false,
+          exploring: false,
+          explored: false,
+          start: start.x === xCol && start.y === yRow ? true : false,
+          end: end.x === xCol && end.y === yRow ? true : false,
+        };
+      }
+    }
+    return tileArray;
+  };
+
+  resetTileArray = () => {
+    const emptyTileArray = this.generateEmptyTileArray();
+    this.setState({ currentGridTiles: emptyTileArray });
+  };
+
+  performFunction = () => {
+    this.resetTileArray();
+  };
+
+  getPath = async () => {
+      let moves = [];
+      const startX = this.state.currentStartingPosition.x;
+      const startY = this.state.currentStartingPosition.y;
+      const endX = this.state.currentEndingPosition.x;
+      const endY = this.state.currentEndingPosition.y;
+      const cloneGrid = this.state.currentGridTiles.slice();
+      let responseDijkstra = await dijkstra(startX, startY, endX, endY, cloneGrid);
+      return responseDijkstra;
+  };
+
+  showMeTheWay = async () => {
+      const moves = await this.getPath();
+      const timer = ms => new Promise(res => setTimeout(res, ms));
+
+      for (let move in moves) {
+          await timer(0.000000001);
+          const newArray = this.updatedTileArray(moves[move].x, moves[move].y, moves[move].state);
+          this.setState({currentGridTiles: newArray});
+      }
+  }
+
+  render() {
+    return (
+      <div className="gridWrapper">
+        <button
+          onClick={() => {
+            this.performFunction();
+          }}
+        >
+          Reset
+        </button>        
+        <div className="grid">
+          {this.state.currentGridTiles &&
+            this.state.currentGridTiles.map((row) => {
+              return row.map((tile) => {
+                const {
+                  key,
+                  x,
+                  y,
+                  handleMouseDown,
+                  handleMouseEnter,
+                  handleMouseUp,
+                  wall,
+                  path,
+                  exploring,
+                  explored,
+                  start,
+                  end,
+                } = tile;
+                return (
+                  <Tile
+                    key={key}
+                    x={x}
+                    y={y}
+                    handleMouseEnter={handleMouseEnter}
+                    wall={wall}
+                    path={path}
+                    exploring={exploring}
+                    explored={explored}
+                    handleMouseDown={handleMouseDown}
+                    handleMouseUp={handleMouseUp}
+                    start={start}
+                    end={end}
+                  />
+                );
+              });
+            })}
+        </div>
+        <button
+          onClick={() => {
+            this.showMeTheWay();
+          }}
+        >
+          Visualize Dijkstra
+        </button>
+      </div>
+    );
+  }
+}
